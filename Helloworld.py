abb88dfef66bd2350ae8c3c99aad0717222844b5
Lasse-Stacher <lasse.stacher@hotmail.com>
2020-12-09 09:56:27 +0100

    Helloworld added

---
+++ Helloworld.py
@@ -0,0 +1,13 @@
+
+#tal:variabel    3:værdi
+tal = 3
+sentence = "this is a sentence"
+
+
+#Function        parameters
+def printToTerminal(parameter, number):
+    print(parameter, number)
+
+printToTerminal("hi there", tal)
+
+
